ea3f56b098111763fb408c76c92efa1bf2e5a4b4
margungijs <margungijs@margungijs.com>
2023-11-01 14:04:16 +0200

    pushed components

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.module.css";
+
+const Title = () => {
+    return(
+        <section>
+            <h1 className = 'title'>Component diagram</h1>
+            <article>
+
+            </article>
+        </section>
+    );
+}
+
+export default Title;
